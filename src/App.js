7485a2914a714e380c187465c2510dca77936615
Badrinathan-S <badrinathan3@gmail.com>
2022-11-15 06:57:13 +0530

    leaflet and maptail integerated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-
+import { useState } from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import './App.css';
+import "leaflet/dist/leaflet.css"
 
 function App() {
 
@@ -11,8 +14,10 @@ function App() {
   function success(pos) {
 
     console.log(pos);
-    // const crd = pos.coords;
-  
+    const crd = pos.coords;
+
+    setCenter({lat: crd.latitude, lng: crd.longitude});
+
     // console.log('Your current position is:');
     // console.log(`Latitude : ${crd.latitude}`);
     // console.log(`Longitude: ${crd.longitude}`);
@@ -25,10 +30,22 @@ function App() {
 
   const myLocation = navigator.geolocation.getCurrentPosition(success, error, options);
 
+  const [center, setCenter] = useState({lat: 0, lng: 0});
+
 
   return (
     <div className="App">
-      Hi
+      <MapContainer center={center} zoom={6} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=rssMvPnGXHosYe9Ybksw"
+        />
+        {/* <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
+      </MapContainer>
     </div>
   );
 }
