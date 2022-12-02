79e7ce9dae01db021b8d954e9bfc92c6afbade42
Badrinathan-S <badrinathan3@gmail.com>
2022-12-02 09:52:55 +0530

    code refactoring!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState, useRef } from 'react';
-import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 import "leaflet/dist/leaflet.css"
 import L from "leaflet";
+import { Container } from 'react-bootstrap';
 
 
 const markerIcon = new L.Icon({
@@ -23,31 +24,31 @@ function App() {
 
   navigator.geolocation.watchPosition((position) => {
 
-    console.log(position.coords)
-    let lat = position.coords.latitude; 
+    // console.log(position.coords)
+    let lat = position.coords.latitude;
     let lng = position.coords.longitude;
 
-    setLatLng({lat: lat, lng: lng})
-  }, (err) => {console.log(err)}, options)
+    setLatLng({ lat: lat, lng: lng })
+  }, (err) => { console.log(err) }, options)
 
-  const [latlng, setLatLng] = useState({lat: 0, lng: 0});
+  const [latlng, setLatLng] = useState({ lat: 0, lng: 0 });
   const [zoomCenter, setZoomCenter] = useState(4);
   const mapRef = useRef();
-
-  
   return (
     <div className="App">
-      <MapContainer center={latlng} zoom={zoomCenter} scrollWheelZoom={true} >
-        <TileLayer
-          attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=rssMvPnGXHosYe9Ybksw"
-        />
-        <Marker position={latlng} icon={markerIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <Container>
+        <MapContainer center={latlng} zoom={zoomCenter} scrollWheelZoom={true} >
+          <TileLayer
+            attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=rssMvPnGXHosYe9Ybksw"
+          />
+          <Marker position={latlng} icon={markerIcon}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </Container>
     </div>
   );
 }
